e0c05227e8a9cda8651b9adec9b1d530cb670efc
jkjarvis <anuttamanand@gmail.com>
2020-05-06 13:02:48 +0530

    changes made

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,19 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+ratings = (
+    (5, 5),
+    (4, 4),
+    (3, 3),
+    (2, 2),
+    (1, 1),
+    (0, 0),
+)
+
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
     link = models.URLField()
     teacher = models.CharField(max_length=100)
     SubjectField = models.CharField(max_length=200, null=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(choices=ratings, default=5)
